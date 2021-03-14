9fa0d255f1d01fff5b1b89a7e66d63ce979f86ea
eli-b-washington <bea14018@byui.edu>
2021-03-13 18:55:56 -0800

    commit on master

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const port = process.env.PORT || 5001;
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 //database connection
 const { Pool } = require('pg');
-const connectionString = process.env.DATABASE_URL || "postgres://hvahobvcltvjgg:9d0f8e0eefaa897c79a4d5380793924691c9c6da11260d8bea8a0cb76a3e45c9@ec2-54-161-239-198.compute-1.amazonaws.com:5432/damrstam69ec9m?ssl=true";
+const connectionString = process.env.DATABASE_URL || 'postgres://lkebzixqtzurfr:fe76c3e49349722290ab6932ca3a0f2067644eb572225fb977ddc5347d566fb1@ec2-54-159-175-113.compute-1.amazonaws.com:5432/d2bfq2b6bdhj2q?ssl=true';
 const pool = new Pool({connectionString: connectionString});
 
 const sql = "SELECT * FROM updates;"
 
-pool.query(sql, function(err, result) {
+pool.query(sql, function(err, res) {
     // If an error occurred...
     if (err) {
         console.log("Error in query: ")
@@ -18,7 +18,7 @@ pool.query(sql, function(err, result) {
 
     // Log this to the console for debugging purposes.
     console.log("Back from DB with result:");
-    console.log(result.rows);
+    console.log(res.rows);
 }); 
 
 // views is directory for all template files
@@ -115,10 +115,7 @@ function computeOperation(response, weight, postage) {
 
 
 
-app.get('/getInfo', handleGetInfo);
-
-
-
+app.get('/handlegetInfo', handleGetInfo);
 
 function handleGetInfo(request, response) {
 	var op = request.query.id;
@@ -127,7 +124,7 @@ function handleGetInfo(request, response) {
 	console.log(op);
 	if (op == 1) {
 		console.log("inside");
-		const sql = "SELECT update FROM updates;"
+		const sql = "SELECT note FROM updates;"
 		console.log(sql);
 		pool.query(sql, function (err, res) {
 			// If an error occurred...
@@ -144,70 +141,11 @@ function handleGetInfo(request, response) {
 				op: op,
 				res: stringres
 			};
-			response.render('final-project', params);
+      console.log(params);
+      
+      response.send(res.rows);
+
 		});
 	}
 }
 
-
-
-/*
-function getInfo(request, response) {
-	// First get the person's id
-	const id = request.query.id;
-
-	// TODO: We should really check here for a valid id before continuing on...
-
-	// use a helper function to query the DB, and provide a callback for when it's done
-	getPersonFromDb(id, function(error, result) {
-		// This is the callback function that will be called when the DB is done.
-		// The job here is just to send it back.
-
-		// Make sure we got a row with the person, then prepare JSON to send back
-		if (error || result == null || result.length != 1) {
-			response.status(500).json({success: false, data: error});
-		} else {
-			const person = result[0];
-			response.status(200).json(person);
-		}
-	});
-}
-
-// This function gets a person from the DB.
-// By separating this out from the handler above, we can keep our model
-// logic (this function) separate from our controller logic (the getPerson function)
-function getInfoFromDb(id, callback) {
-	console.log("Getting person from DB with id: " + id);
-
-	// Set up the SQL that we will use for our query. Note that we can make
-	// use of parameter placeholders just like with PHP's PDO.
-	const sql = "SELECT id, first, last, birthdate FROM person WHERE id = $1::int";
-
-	// We now set up an array of all the parameters we will pass to fill the
-	// placeholder spots we left in the query.
-	const params = [id];
-
-	// This runs the query, and then calls the provided anonymous callback function
-	// with the results.
-	pool.query(sql, params, function(err, result) {
-		// If an error occurred...
-		if (err) {
-			console.log("Error in query: ")
-			console.log(err);
-			callback(err, null);
-		}
-
-		// Log this to the console for debugging purposes.
-		console.log("Found result: " + JSON.stringify(result.rows));
-
-
-		// When someone else called this function, they supplied the function
-		// they wanted called when we were all done. Call that function now
-		// and pass it the results.
-
-		// (The first parameter is the error variable, so we will pass null.)
-		callback(null, result.rows);
-	});
-
-} 
-*/
